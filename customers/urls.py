a3e5cb50ad7652ba08e1e0db820d876916663d00
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-02-24 16:02:01 -0800

    asic work with backend Customer Implement

--- customers/urls.py
+++ customers/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from customers import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/customers/', views.customers, name='customers'),
+    path('api/customers/<int:id>', views.customer, name='customer'),
 ]
